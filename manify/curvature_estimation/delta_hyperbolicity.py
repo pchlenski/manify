fac7a66812feff4ad39b12776865ea50c30e5491
DylanSatow <dylansatow531@gmail.com>
2025-03-24 02:29:56 -0400

    Did experiments and added relative deltas

--- manify/curvature_estimation/delta_hyperbolicity.py
+++ manify/curvature_estimation/delta_hyperbolicity.py
@@ -25,27 +25,55 @@ def delta_hyperbolicity(dismat: Float[torch.Tensor, "n_points n_points"]) -> Flo
 
     out = torch.minimum(XY_p_xy, XY_p_yz)
     
-    out = (out - XY_p_xz)
-    return out.max().item()
+    delta = (out - XY_p_xz).max()
+    
+    diam = torch.max(dismat)
+
+    rel_delta = 2 * delta / diam
+
+    return rel_delta.item()
 
 
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# def delta_full(dismat):
-#     # We are getting (y,z)_x = .5 (d(x,y) + d(x,z) - d(y,z))
-#     n = dismat.shape[0]
-#     p = 0  # Fix w_0
-#     d_xp = dismat[p, :].unsqueeze(0)  # (1,n)
-#     d_yp = dismat[:, p].unsqueeze(1)  # (n,1)
-#     # (n,n) matrix of all pairwise gromov products for fixed point
-#     XY_p = .5 * (d_xp + d_yp - dismat)
+def delta_full(dismat):
+    # We are getting (y,z)_x = .5 (d(x,y) + d(x,z) - d(y,z))
+    n = dismat.shape[0]
+    p = 0  # Fix w_0
+    d_xp = dismat[p, :].unsqueeze(0)  # (1,n)
+    d_yp = dismat[:, p].unsqueeze(1)  # (n,1)
+    # (n,n) matrix of all pairwise gromov products for fixed point
+    XY_p = .5 * (d_xp + d_yp - dismat)
 
-#     XY_p_xy = XY_p.unsqueeze(2).expand(-1, -1, n)  # (n,n,n)
-#     XY_p_yz = XY_p.unsqueeze(0).expand(n, -1, -1)  # (n,n,n)
-#     XY_p_xz = XY_p.unsqueeze(1).expand(-1, n, -1)  # (n,n,n)
+    XY_p_xy = XY_p.unsqueeze(2).expand(-1, -1, n)  # (n,n,n)
+    XY_p_yz = XY_p.unsqueeze(0).expand(n, -1, -1)  # (n,n,n)
+    XY_p_xz = XY_p.unsqueeze(1).expand(-1, n, -1)  # (n,n,n)
 
-#     # Return the 3-tensor of delta values before taking the max
-#     minmax = torch.minimum(XY_p_xy, XY_p_yz)
-#     return minmax - XY_p_xz
+    # Return the 3-tensor of delta values before taking the max
+    minmax = torch.minimum(XY_p_xy, XY_p_yz)
+    
+    deltas = minmax - XY_p_xz
+    return deltas
+
+def rel_deltas_full(dismat):
+    # We are getting (y,z)_x = .5 (d(x,y) + d(x,z) - d(y,z))
+    n = dismat.shape[0]
+    p = 0  # Fix w_0
+    d_xp = dismat[p, :].unsqueeze(0)  # (1,n)
+    d_yp = dismat[:, p].unsqueeze(1)  # (n,1)
+    # (n,n) matrix of all pairwise gromov products for fixed point
+    XY_p = .5 * (d_xp + d_yp - dismat)
+
+    XY_p_xy = XY_p.unsqueeze(2).expand(-1, -1, n)  # (n,n,n)
+    XY_p_yz = XY_p.unsqueeze(0).expand(n, -1, -1)  # (n,n,n)
+    XY_p_xz = XY_p.unsqueeze(1).expand(-1, n, -1)  # (n,n,n)
+
+    # Return the 3-tensor of delta values before taking the max
+    minmax = torch.minimum(XY_p_xy, XY_p_yz)
+    
+    deltas = minmax - XY_p_xz
+    diam = torch.max(dismat)
+    rel_deltas = 2 * deltas / diam
+    return rel_deltas
 
 # def batched_delta_hyp(X: torch.Tensor, n_tries=10, batch_size=1500):
 #     deltas = []
